a01db229c75d8fc20934a43e588297b33857395d
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2018-11-25 18:12:19 +0200

    add input field

---
+++ src/components/InputForm/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import classes from './style.module.css';
+
+class InputForm extends Component {
+  state = {
+    content: ''
+  }
+
+  changeHandler = event => {
+    this.setState({content: event.target.value});
+  }
+
+  render() {
+    const { content } = this.state;
+    return (
+      <div>
+        <form className={classes.form}>
+          <label>Search for a medicine:</label>
+          <input
+            type="text"
+            placeholder="e.g. salbutamol or asthavent"
+            value={content}
+            onChange={this.changeHandler}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default InputForm;
