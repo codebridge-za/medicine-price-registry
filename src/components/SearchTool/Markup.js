3c870c93bca33bc83bcb496580c13e35ca0445b0
ZeeshaanMaudar <maudarbocus.zeeshaan@gmail.com>
2019-03-16 17:21:31 +0200

    refactor Markup for SearchTool

--- src/components/SearchTool/Markup.js
+++ src/components/SearchTool/Markup.js
@@ -1,61 +1,23 @@
 import React from 'react';
-// import { uniqBy } from 'lodash';
 import MedicineBasicSearch from '../MedicineBasicSearch';
 
 import classes from './style.module.css';
 
-// class SearchTool extends Component {
-//   state = {
-//     content: '',
-//     results: [],
-//   }
+const callForm = (submitForm, content, changeHandler) => (
 
-//   fetchData = (content) => {
-//     fetch(`https://mpr.code4sa.org/api/v2/search?q=${content}`)
-//       .then(response => response.json())
-//       .then((parsedJSON) => {
-//         const results = uniqBy(parsedJSON, 'nappi_code');
-//         return this.setState({ results });
-//       });
-//   }
-
-//   submitForm = (event) => {
-//     event.preventDefault();
-//     this.setState({ content: '' });
-//   }
-
-//   changeHandler = (event) => {
-//     this.setState({ content: event.target.value }, () => {
-//       const { content } = this.state;
-//       if (content && content.length > 3) {
-//         this.fetchData(content);
-//       }
-//     });
-//   }
-
-//   render() {
-//     const { content, results } = this.state;
-//     return (
-//       <React.Fragment>
-//         <form className={classes.form} onSubmit={this.submitForm}>
-//           <label htmlFor="medicine">
-//             <span>Search for a medicine:</span>
-//             <input
-//               id="medicine"
-//               type="text"
-//               placeholder="e.g. salbutamol or asthavent"
-//               value={content}
-//               onChange={this.changeHandler}
-//             />
-//           </label>
-//         </form>
-//         <MedicineBasicSearch results={results} />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-// export default SearchTool;
+  <form className={classes.form} onSubmit={submitForm}>
+    <label htmlFor="medicine">
+      <span>Search for a medicine:</span>
+      <input
+        id="medicine"
+        type="text"
+        placeholder="e.g. salbutamol or asthavent"
+        value={content}
+        onChange={changeHandler}
+      />
+    </label>
+  </form>
+);
 
 const Markup = (props) => {
   const {
@@ -67,18 +29,7 @@ const Markup = (props) => {
 
   return (
     <React.Fragment>
-      <form className={classes.form} onSubmit={submitForm}>
-        <label htmlFor="medicine">
-          <span>Search for a medicine:</span>
-          <input
-            id="medicine"
-            type="text"
-            placeholder="e.g. salbutamol or asthavent"
-            value={content}
-            onChange={changeHandler}
-          />
-        </label>
-      </form>
+      {callForm(submitForm, content, changeHandler)}
       <MedicineBasicSearch results={results} />
     </React.Fragment>
   );
