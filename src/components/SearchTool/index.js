5bb8a87cc1318a3d5b6dde945e292ed2b58ec2b3
Musango <musangow@gmail.com>
2019-03-20 08:33:13 +0200

    Added Loading animation
    updated git ignore file to ignore .idea (jetbrain users)
    created loader component
    added loader component to SearchTool/index.js. Loader shows while waiting to get data
    added some error handling if your request fails

--- src/components/SearchTool/index.js
+++ src/components/SearchTool/index.js
@@ -2,25 +2,57 @@ import React, { Component } from 'react';
 import MedicineBasicSearch from '../MedicineBasicSearch';
 
 import classes from './style.module.css';
+import Loader from '../Loader';
+
+const BaseRequest = {
+  loading: false,
+  error: false,
+  success: false,
+  errMessage: '',
+};
 
 class SearchTool extends Component {
   state = {
     content: '',
     results: [],
-  }
+    request: {
+      ...BaseRequest,
+    },
+
+  };
+
+  updateRequest = obj => this.setState({
+    request: {
+      ...BaseRequest,
+      ...obj,
+    },
+  });
 
   fetchData = (content) => {
+    this.updateRequest({ loading: true });
     fetch(`https://mpr.code4sa.org/api/v2/search?q=${content}`)
       .then(response => response.json())
-      .then(parsedJSON => (
-        this.setState({ results: parsedJSON })
-      ));
-  }
+      .then((parsedJSON) => {
+        this.setState({
+          results: parsedJSON,
+          request: {
+            ...BaseRequest,
+            success: true,
+          },
+        });
+      })
+      .catch(() => {
+        this.updateRequest({
+          error: true,
+          errMessage: 'Oh no something went wrong',
+        });
+      });
+  };
 
   submitForm = (event) => {
     event.preventDefault();
     this.setState({ content: '' });
-  }
+  };
 
   changeHandler = (event) => {
     this.setState({ content: event.target.value }, () => {
@@ -29,25 +61,44 @@ class SearchTool extends Component {
         this.fetchData(content);
       }
     });
-  }
+  };
 
   render() {
-    const { content, results } = this.state;
+    const {
+      content, results, request: {
+        loading,
+        error,
+        success,
+        errMessage,
+      },
+    } = this.state;
     return (
       <React.Fragment>
         <form className={classes.form} onSubmit={this.submitForm}>
           <label htmlFor="medicine">
             <span>Search for a medicine:</span>
-            <input
-              id="medicine"
-              type="text"
-              placeholder="e.g. salbutamol or asthavent"
-              value={content}
-              onChange={this.changeHandler}
-            />
+
+            <div className={classes.inputContainer}>
+              <input
+                id="medicine"
+                type="text"
+                placeholder="e.g. salbutamol or asthavent"
+                value={content}
+                onChange={this.changeHandler}
+              />
+            </div>
           </label>
         </form>
-        <MedicineBasicSearch results={results} />
+
+        {loading && (
+        <div className={classes.loaderContainer}>
+          <Loader />
+        </div>
+        )}
+        {success && <MedicineBasicSearch results={results} />}
+
+        {error && <div>{errMessage}</div>}
+
       </React.Fragment>
     );
   }
