b5b169089b0fccd799262cedf7a4e00c669225ae
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-05-28 09:51:23 +0200

    transitioning to styled components

---
+++ src/components/MedicineBasicSearch/styled.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+import {
+  Card,
+  Typography,
+  IconButton,
+  Button,
+} from '@material-ui/core';
+
+const CardStyled = styled(Card)``;
+
+
+const Title = styled(Typography)``;
+
+
+const Price = styled(Typography)``;
+
+
+const ButtonStyled = styled(Button)``;
+
+
+const IconButtonStyled = styled(IconButton)``;
+
+export {
+  CardStyled,
+  Title,
+  IconButtonStyled,
+  Price,
+  ButtonStyled,
+};
+
+export default {
+  CardStyled,
+  Title,
+  IconButtonStyled,
+  Price,
+  ButtonStyled,
+};
